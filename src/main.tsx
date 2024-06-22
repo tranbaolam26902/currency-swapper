e7845576f40378efd4d0c82660c9f5586cec756f
Campbells <tranbaolam26902@gmail.com>
2024-06-22 16:19:59 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import { store } from './redux/store.tsx';
+import { store } from '@redux/store.tsx';
 
-import App from './App.tsx';
+import App from '@/App.tsx';
 
-import './index.css';
+import '@/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
